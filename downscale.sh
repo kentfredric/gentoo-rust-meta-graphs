6b2df89df2a15015c43ca674662fe24eaa40e5f9
Kent Fredric <kentnl@gentoo.org>
2020-02-27 01:02:51 +1300

    Do downscale concurrently

--- downscale.sh
+++ downscale.sh
@@ -1,8 +1,19 @@
+mk_thumbnail() {
+  local base="${1}"
+  local source="${1}.png"
+  local target="${2:-${1}_thumb.jpg}"
+
+  printf "\e[32m* Scaling %s to %s ...\e[0m\n" "${source}" "${target}"
+  pngtopam -verbose "${source}" |
+    pamscale -verbose -xyfit 640 480 -filter sinc |
+    pnmtojpeg -verbose -quality 85 -optimize -progressive -dct=float > "${target}"
+  printf "\e[32m* Done %s -> %s [^_^]\e[0m\n" "${source}" "${target}"
+}
+
+
 for model in heirarchic radial; do
   for form in "" "_notest"; do
-    printf "\e[32m* Scaling %s.png to %s_thumb.jpg ...\e[0m\n" "${model}${form}" "${model}${form}"
-    pngtopam -verbose "${model}${form}".png |
-      pamscale -verbose -xyfit 640 480 -filter sinc |
-      pnmtojpeg -verbose -quality 85 -optimize -progressive -dct=float > "${model}${form}"_thumb.jpg
-  done
+    mk_thumbnail "${model}${form}" &
+   done
 done
+wait
